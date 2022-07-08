c997a1311afcb2db89a0b0499c49262170aef878
Oudom <rithoudom168@gmail.com>
2022-07-08 15:04:38 +0700

    edit all Ex1 - Ex2 code

--- sort_function.py
+++ sort_function.py
@@ -4,4 +4,7 @@
 
 number_list = [100,55,70,80,-100,8.5,13,2022,-2025]
 number_list.sort()
-print(number_list)
+print("Samll to big: ",number_list)
+print("##################")
+number_list.sort(reverse=True)
+print("Big to small: ",number_list)
