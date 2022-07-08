706a69555e3ef5ce170ea7af5b86e0e6ebe59e25
Oudom <rithoudom168@gmail.com>
2022-07-08 14:44:17 +0700

    update code

--- Prime_number.py
+++ Prime_number.py
@@ -3,16 +3,22 @@
 # 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
 # Prime number are 2, 3, 5, 7 Ex: 2 = 2x1, 3 = 3x1, 5 = 5x1, 7 = 7x1
 
-# like 4 = 2x2 = 4x1
+# like 4 = 2x2 = 4x1 
 # like 6 = 2x3 = 6x1
 # like 8 = 2X2x2 = 2x4 = 8x1
-
+# Note: every prime number are division of non prime number 
 #############################
 
-Lower_interval = int(input('Enter Number = '))
-Last_interval = int(input('Enter Number = '))
+Lower_interval = int(input('Enter Lower Number = '))
+Last_interval = int(input('Enter Last Number = '))
+
+for prime_number in range (Lower_interval, Last_interval+1):
+    if prime_number > 1:
+        for division in range (2, prime_number):
+            if (prime_number%division) == 0:
+                break
+        else:
+            print(prime_number)
 
 
 ############################
-print(Lower_interval)
-print(Last_interval)
