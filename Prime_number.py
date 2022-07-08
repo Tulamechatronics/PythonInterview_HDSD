1b727a6542b78de5e89df3239327b1d91664744f
Oudom <rithoudom168@gmail.com>
2022-07-08 14:25:04 +0700

    prime number

---
+++ Prime_number.py
@@ -0,0 +1,18 @@
+
+# what is prime number?
+# 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+# Prime number are 2, 3, 5, 7 Ex: 2 = 2x1, 3 = 3x1, 5 = 5x1, 7 = 7x1
+
+# like 4 = 2x2 = 4x1
+# like 6 = 2x3 = 6x1
+# like 8 = 2X2x2 = 2x4 = 8x1
+
+#############################
+
+Lower_interval = int(input('Enter Number = '))
+Last_interval = int(input('Enter Number = '))
+
+
+############################
+print(Lower_interval)
+print(Last_interval)
